ca7048a34b4f9922f9fa7f8cc5b38b1d4642e10f
yanglinsensjtu <yanglinsensjtu@126.com>
2019-09-14 07:32:05 +0800

    import data and tidy

---
+++ data.R
@@ -0,0 +1,39 @@
+library(readxl)
+library(readr)
+library(purrr)
+library(stringr)
+library(pryr)
+MS110 <- read_excel('../20190725 YLS SILAC/20190725YLS proteinGroups_110.xlsx')
+MS211 <- read_excel('../20190725 YLS SILAC/20190725 YLSproteinGroups_211.xlsx')
+MS312 <- read_excel('../20190725 YLS SILAC/20190725 YLS proteinGroups_312.xlsx')
+MS74 <- read_excel('../20190725 YLS SILAC/20190725YLS proteinGroups 74.xlsx')
+MS85 <- read_excel('../20190725 YLS SILAC/20190725YLS proteinGroup 85.xlsx')
+MS96 <- read_excel('../20190725 YLS SILAC/20190725YLS ProteinGroup_96.xlsx')
+
+extract.protein.name<- function(x){
+  p <- parent.frame()
+  if(is.data.frame(x)){
+    name <- deparse(substitute(x))
+    x$`Protein IDs` <- str_extract(x$`Protein IDs`, pattern = '(?<=\\|)\\w+(?=\\_HUMAN)')
+    if(exists(name, envir = p, inherits = F)){
+      assign(name, x, envir = p)
+    } else {
+      assign(name, x, envir = where(name))
+      }
+  }else if(is.character(x)){
+    name <- x
+    obj <- get(x, envir = p)
+    obj$`Protein IDs` <- str_extract(obj$`Protein IDs`, pattern = '(?<=\\|)\\w+(?=\\_HUMAN)')
+    if (exists(name, envir = p, inherits = F)) {
+      assign(name, obj, envir = p)
+    }else{
+      assign(name, obj, envir = where(name))
+    }
+  } else {
+    errorCondition("The type of argument should be 'character' or 'dataframe'")
+  }
+}
+data.obj <- ls(pattern = 'MS*')
+lapply(data.obj, extract.protein.name)
+
+
